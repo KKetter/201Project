5f24b15f32b653b23e7c10047c3514c3a15b5bad
Deziree Teague <dez.teague@gmail.com>
2018-11-12 14:54:27 -0800

    updated buttons, text colors, and table size

--- app.js
+++ app.js
@@ -1,1 +1,115 @@
+'use-script';
 
+var category = ['carbs', 'prot', 'fats'];
+
+var userInput0 = [];
+var userInput1 = [];
+var userInput2 = [];
+console.log(userInput0);
+console.log(userInput1);
+console.log(userInput2);
+var ctx = document.getElementById('myChart').getContext('2d'); //hook for chart
+
+function Macros(newCarbs, newProt, newFats) {
+  
+  this.newCarbs = newCarbs;
+  this.newProt = newProt;
+  this.newFats = newFats;
+  userInput0.push(this.newCarbs);
+  userInput1.push(this.newProt);
+  userInput2.push(this.newFats);
+  renderChart();
+}
+
+Macros.prototype.storeMacros = function () {
+  for (var i = 0; i < category.length; i++) {
+    var singleInput = this.newCarbs + this.newProt + this.newFats;
+    this.userInput.push(singleInput);
+  }
+}
+function renderChart() {
+  // var votedArray = []; //defined array to tallys votes before refresh
+  // var votesArray = []; //defined array for votes each render results
+  // if (localStorage.getItem('votes')) {
+  //   var votesData = localStorage.getItem('votes');
+  //   votedArray = JSON.parse(votesData); //pulls from local storage and pushes into defined array
+  //   for ( var h = 0; h < products.length; h++) {
+  //     votedArray[h] = parseInt(votedArray[h]);
+  //     votes[h] += votedArray[h];
+  //   }
+  // }
+  var userInput = [userInput0, userInput1, userInput2];
+  // console.log(userInputResults);
+  // for (var j = 0; j < userInput.length; j++) {
+  //   userInputResults.push(userInput[j].userInputResults);
+  // }
+  var chartConfig = { //defined variable to hold chart properties
+    type: 'horizontalBar',
+    data: {
+      labels: category,
+      datasets: [{
+        label: ' Total Macros',
+        data: userInput,
+        color: '#000000',
+        backgroundColor: [
+          'rgba(125,249,255)',
+          'rgba(153,102,204)',
+          'rgba(255,51,204)',
+          'rgba(0,255,255)',
+          'rgba(42,82,190)',
+          'rgba(253,238,0)',
+          'rgba(63,0,255)',
+          'rgba(102,255,0)',
+          'rgba(0,20,168)',
+          'rgba(255,56,0)',
+          'rgba(0,255,0)',
+          'rgba(125,249,255)',
+          'rgba(255,255,0)',
+          'rgba(244,0,161)',
+          'rgba(255,191,0)',
+          'rgba(39,59,226)',
+          'rgba(253,14,53)',
+          'rgba(255,250,250)',
+          'rgba(0,250,154)',
+          'rgba(204,255,0)',],
+        borderColor: [],
+        borderWidth: 2
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          plotOptions: {
+            series: {
+              groupPadding: 0
+            }
+          },
+          barPercentage: 2.0,
+          categoryPercentage: .5,
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  };
+  return new Chart(ctx, chartConfig);
+// var voteData = JSON.stringify(votes); //pushes data into local storage before refresh
+// votesArray.push(voteData);
+// localStorage.setItem('votes', votesArray);
+}
+renderChart();
+var formReset = document.getElementById('form-data');
+formReset.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var newCarbs = event.target.newCarbs.value;
+  var newProt = event.target.newProt.value;
+  var newFats = event.target.newFats.value;
+  event.target.store = new Macros(newCarbs, newProt, newFats);
+  event.target.newCarbs.value = '';
+  console.log(newCarbs);
+  event.target.newProt.value = '';
+  console.log(newProt);
+  event.target.newFats.value = '';
+  console.log(newFats);
+});
