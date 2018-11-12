11aa2c57539f2ee13353d54888bed351b25e3a4b
ntibbals <ntibbals@outlook.com>
2018-11-12 10:49:01 -0800

    working on event listeners

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+'use-script';
+
+var category = ['carbs', 'prot', 'fats'];
+var userInput = [];
+console.log(userInput);
+
+function Macros(newCarbs, newProt, newFats) {
+  this. newCarbs = newCarbs;
+  this.newProt = newProt;
+  this.newFats = newFats;
+  this.category = category;
+  // this.quantity = quantity;
+  userInput.push(this);
+}
+
+// Macros.prototype.storeMacros = function () {
+//   var 
+// }
+var formReset = document.getElementById('form-data');
+formReset.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var newCarbs = event.target.newCarbs.value;
+  var newProt = event.target.newProt.value;
+  var newFats = event.target.newFats.value;
+  event.target.store = new Macros(newCarbs, newProt, newFats);
+  event.target.newCarbs.value = '';
+  console.log(newCarbs);
+  event.target.newProt.value = '';
+  console.log(newProt);
+  event.target.newFats.value = '';
+  console.log(newFats);
+});
 
