5ef00b2016bffd317fd6815561716941d0436895
ntibbals <ntibbals@outlook.com>
2018-11-13 12:01:50 -0800

    completed 1st chart, local storage and reset button working, building out water chart

--- app.js
+++ app.js
@@ -1,52 +1,59 @@
 'use strict';
-var category = ['carbs', 'prot', 'fats'];
+var category1 = ['carbs', 'prot', 'fats'];
+var category2 = ['water'];
 var userInput0 = [];
 var userInput1 = [];
 var userInput2 = [];
+var waterInput = [];
+var currentWaterInput = [];
 var currentInput = [];
 var macrosArray = [];
-var ctx = document.getElementById('myChart').getContext('2d'); //hook for chart
-function Macros(newCarbs, newProt, newFats) {
+var ctx = document.getElementById('myChart').getContext('2d');
+var ctx2 = document.getElementById('myChart2').getContext('2d'); //hook for chart
+function Macros(newCarbs, newProt, newFats, newWater) {
   this.newCarbs = newCarbs;
   this.newProt = newProt;
   this.newFats = newFats;
+  this.newWater = newWater;
   userInput0.push(parseInt(this.newCarbs));
   userInput1.push(parseInt(this.newProt));
   userInput2.push(parseInt(this.newFats));
-  currentInput = [parseInt(newCarbs), parseInt(newProt), parseInt(newFats)];
+  waterInput.push(parseInt(this.newWater));
+  currentWaterInput = [parseInt(newWater)];
+  currentInput = [parseInt(newCarbs), parseInt(newProt), parseInt(newFats),];
   macrosArray.push(this);
 }
 function renderChart() {
   var userInputResults = [];
-  for (var j = 0; j < category.length; j++) {
+  for (var j = 0; j < category1.length; j++) {
     userInputResults.push(currentInput[j]);
   }
   var resultsPrior = [];
-  console.log('results Array', resultsPrior);
+  // console.log('results Array', resultsPrior);
   var postResults = [];
-  console.log('postResults', typeof postResults);
-  console.log('stored results', postResults);
-  console.log('userInputResults', userInputResults);
-  console.log('userInput0', userInput0);
-  console.log('userInput1', userInput1);
-  console.log('userInput2', userInput2);
+  // console.log('postResults', typeof postResults);
+  // console.log('stored results', postResults);
+  // console.log('userInputResults', userInputResults);
+  // console.log('userInput0', userInput0);
+  // console.log('userInput1', userInput1);
+  // console.log('userInput2', userInput2);
   if (localStorage.getItem('userInput')) {
     var storedData = localStorage.getItem('userInput');
-    console.log('storedData', storedData);
+    // console.log('storedData', storedData);
     postResults = JSON.parse(storedData);
-    console.log('postResults', postResults);
-    var intPostResults = Array(3);
-    for (var h = 0; h < category.length; h++) {
+    // console.log('postResults', postResults);
+    var intPostResults = Array(4);
+    for (var h = 0; h < category1.length; h++) {
       intPostResults[h] = parseInt(postResults[h]);
-      console.log('intpostresult', intPostResults);
+      // console.log('intpostresult', intPostResults);
       userInputResults[h] += parseInt(intPostResults[h]);
     }
-    console.log('userInputResults', userInputResults);
+    // console.log('userInputResults', userInputResults);
   }
   var chartConfig = { //defined variable to hold chart properties
     type: 'horizontalBar',
     data: {
-      labels: category,
+      labels: category1,
       datasets: [{
         label: ' Total Macros',
 
@@ -56,22 +63,7 @@ function renderChart() {
           'rgba(125,249,255)',
           'rgba(153,102,204)',
           'rgba(0,255,255)',
-          'rgba(42,82,190)',
-          'rgba(253,238,0)',
-          'rgba(63,0,255)',
-          'rgba(102,255,0)',
-          'rgba(0,20,168)',
-          'rgba(255,56,0)',
-          'rgba(0,255,0)',
-          'rgba(125,249,255)',
-          'rgba(255,255,0)',
-          'rgba(244,0,161)',
-          'rgba(255,191,0)',
-          'rgba(39,59,226)',
-          'rgba(253,14,53)',
-          'rgba(255,250,250)',
-          'rgba(0,250,154)',
-          'rgba(204,255,0)',],
+          'rgba(42,82,190)',],
         borderColor: [],
         borderWidth: 2
       }]
@@ -94,21 +86,96 @@ function renderChart() {
   };
   var resultsData = JSON.stringify(userInputResults); //pushes data into local storage before refresh
   resultsPrior.push(resultsData);
-  console.log('results data',typeof resultsData);
-  console.log('results data',typeof resultsPrior);
+  // console.log('results data',typeof resultsData);
+  // console.log('results data',typeof resultsPrior);
   localStorage.setItem('userInput', resultsData);
   return new Chart(ctx, chartConfig);
 }
-// renderChart();
-var formReset = document.getElementById('form-data');
-formReset.addEventListener('submit', function (event) {
+function renderWaterChart() {
+  var waterResults = [];
+  for (var j = 0; j < category2.length; j++) {
+    waterResults.push(parseInt(currentWaterInput[j]));
+    console.log('water for loop', waterResults);
+  }
+  console.log('water results Array', waterResults);
+  var waterPostResults = [];
+  console.log('waterPostResults', typeof waterPostResults);
+  console.log('water stored results', waterPostResults);
+  if (localStorage.getItem('userInput')) {
+    var waterStoredData = localStorage.getItem('userInput');
+    console.log('waterStoredData', waterStoredData);
+    waterPostResults = JSON.parse(waterStoredData);
+    console.log('waterPostResults', waterPostResults);
+    var intWaterPostResults = Array(1);
+    for (var h = 0; h < category2.length; h++) {
+      intWaterPostResults[h] = parseInt(waterPostResults[h]);
+      console.log('intpostresult', intWaterPostResults);
+      waterPostResults[h] += parseInt(intWaterPostResults[h]);
+    }
+    console.log('waterPostResults', waterPostResults);
+  }
+  var chartConfig = { //defined variable to hold chart properties
+    type: 'bar',
+    data: {
+      labels: category2,
+      datasets: [{
+        label: ' Total Water',
+
+        data: waterResults,
+        color: '#000000',
+        backgroundColor: ['rgba(42,82,190)',],
+        borderColor: [],
+        borderWidth: 2
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          plotOptions: {
+            series: {
+              groupPadding: 0
+            }
+          },
+          barPercentage: 2.0,
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  };
+  var waterResultsData = JSON.stringify(waterResults); //pushes data into local storage before refresh
+  console.log('stringify', waterResultsData);
+  localStorage.setItem('waterResults', waterResultsData);
+  waterResults.push(waterResultsData);
+  console.log('water results data', waterResultsData);
+  console.log('water results data', waterResults);
+  return new Chart(ctx2, chartConfig);
+}
+var formSubmit = document.getElementById('form-data');
+formSubmit.addEventListener('submit', function (event) {
   event.preventDefault();
   var newCarbs = event.target.newCarbs.value;
   var newProt = event.target.newProt.value;
   var newFats = event.target.newFats.value;
-  new Macros(newCarbs, newProt, newFats);
+  var newWater = event.target.newWater.value;
+  new Macros(newCarbs, newProt, newFats, newWater);
   renderChart();
+  renderWaterChart();
   event.target.newCarbs.value = '';
   event.target.newProt.value = '';
   event.target.newFats.value = '';
+  event.target.newWater.value = '';
 });
+var clearOut = function() {
+  var reset = document.getElementById('reset-data');
+  reset.addEventListener ('click', function() {
+    if (window.confirm('Do you really want to clear your macros?')) {
+      localStorage.clear();
+      location.reload();
+    } else {
+      return;
+    }
+  });
+};
+clearOut();
